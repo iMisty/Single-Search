c4c2e6a929c46248f52ddd69d50c681cbc6476a4
Jingyi21 <qianwumeng_2cyndi@qq.com>
2018-09-08 00:46:19 +0800

    稳定上线 v1.0

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
+ //import { frontWorldOption,deepWorldOption } from './option';
+
  $(document).ready(function(){
      changeBG();
-     $('a#dde').click(function(){
-        $('#deep-dark-world').fadeIn();
-        goToDeepWorld();
-     });
-     $('a#bw').click(function(){
-        backWorld();
-     })
-     
+     frontWorld(); 
  });
 function changeBG(){
     const BG = ['https://ww1.sinaimg.cn/large/0060lm7Tly1fjfi896lfbj31hc0u079s.jpg',
@@ -25,12 +20,85 @@ function changeBG(){
     $('html').css("background-image",a);
 }
 
+function frontWorld(){
+    const template = `<div class="wrap flex" id="normal-world">
+    <h1 class="primary-title">Miya Neko House</h1>
+    <h2 class="secend-title">Moeist</h2>
+        <ul class="flex"><li>
+                <a href="https://miya.moe" target="_blank">
+                    <span class="fa fa-home"></span>
+                    <span>Blog</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/jingyi21" target="_blank">
+                    <span class="fa fa-github"></span>
+                    <span>GitHub</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/KannadukiShizuE" target="_blank">
+                    <span class="fa fa-twitter"></span>
+                    <span>Twitter</span>
+                </a>
+            </li>
+        </ul>
+        <li>
+            <a href="#" class="active" id="dde">
+                <span class="fa fa-plane"></span>
+                <span>Deep Dark Extra</span>
+            </a>
+        </li>
+    </div>`;
+    $('#deep-dark-world').remove();
+    $('body').css('background-color','transparent');
+    $('#app').html(template);
+    $('#normal-world').addClass('animated fadeIn');
+    $('a#dde').click(function(){
+        goToDeepWorld();
+     });    
+}
+
 function goToDeepWorld(){
-    $('#normal-world').addClass('hide');
-    $('#deep-dark-world').removeClass('hide');
-    $('#bw').removeClass('hide')
-            .addClass('active');
-    $('body').css('background-color','rgba(0,0,0,1)');
+    const template = `<div class="wrap flex" id="deep-dark-world">
+    <h2 class="primary-title">里世界魔法屋</h2>
+    <h3>进来魔法屋,就得完全听我的指示</h3>
+    <h3>我选出符合条件的人</h3>
+    <h3>你来决定他们的命运</h3>
+    <ul class="flex">
+        <li>
+            <a href="https://blog.shizu17.pw" target="_blank">
+                <span class="fa fa-home"></span>
+                <span>裏・Blog</span>
+            </a>
+        </li>
+        <li>
+            <a href="https://blog.shizu17.pw/index.php/archives/3.html" target="_blank">
+                <span class="fa fa-paper-plane"></span>
+                <span>ShadowSocksRR</span>
+            </a>
+        </li>
+        <li>
+            <a href="https://ws3.sinaimg.cn/large/0065BpO8gy1fuwmt11x8gj30dq0af3za.jpg" target="_blank">
+                <span class="fa fa-star"></span>
+                <span>魔法屋</span>
+            </a>
+        </li>
+    </ul>
+    <h3>嘿,轮到你了</h3>
+    <li>
+        <a href="#" id="bw">
+            <span class="fa fa-plane"></span>
+            <span>Back World</span>
+        </a>
+    </li>
+</div>`;
+    $('#normal-world').remove();
+    $('#app').html(template);
+    $('#deep-dark-world').addClass('animated fadeIn');
+    $('a#bw').click(function(){
+        frontWorld();
+     })
 }
 
 function backWorld(){
