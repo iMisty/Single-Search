3691f92a89e8f67527436799e753ac6d1de731e5
Miya <qianwumeng_2cyndi@qq.com>
2018-10-23 01:10:33 +0800

    Update components/js/function.js

--- components/js/function.js
+++ components/js/function.js
@@ -33,7 +33,3 @@ if(removeRibbon.removeFork == true){
 if(removeRibbon.removeHitokoto == true){
     $('#hitokoto').remove();
 }
-
-if(window.console){
-    console.log('\n %c TY-Landing Page v1.3.0 %c by Kannazuki Miya |  https://miya.moe ','color:#dcf2f8;background:#23b7e5;padding:5px 0;','color:#eee;background:rgba(252,248,227,.3);padding:5px 0;')
-}
