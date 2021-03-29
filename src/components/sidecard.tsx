45c0cbe9b7b40223df60bdd32766b040508b9837
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-03-29 10:18:02 +0800

    remove

--- src/components/sidecard.tsx
+++
@@ -1,9 +0,0 @@
-import { Component, Vue, Prop } from 'vue-property-decorator';
-@Component({})
-export default class SideCard extends Vue {
-  private render() {
-    return <div class={`mermaid__side-card`}>
-      {this.$slots.default}
-    </div>;
-  }
-}
